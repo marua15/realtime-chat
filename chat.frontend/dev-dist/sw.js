5945f3ebbe69b1804b0b2023b811a27619adb0de
maroua <marouaamal2002@gmail.com>
2023-06-13 18:47:18 +0100

    commit

--- chat.frontend/dev-dist/sw.js
+++ chat.frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vrtv55007vo"
+    "revision": "0.4h7k9u6k6to"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
