4cc9d3b2d7771aa6fb4410785a350ae4cb3b35a9
HAITAME <haitame.laframe@etu.uae.ac.ma>
2023-06-14 11:30:18 +0100

    chat PAGE

--- chat.frontend/src/components/chat/Chat.jsx
+++ chat.frontend/src/components/chat/Chat.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import SendMessageForm from '../SendMessageForm/SendMessageForm';
 import MessageContainer from '../MessageContainer/MessageContainer';
 import ConnectedUsers from '../ConnectedUsers/ConnectedUsers';
-import { Button } from 'react-bootstrap';
-import './Chat.scss'
-const Chat = ({  messages,sendMessage, users, closeConnection }) => (
-  <div>
-    <div className='leave-room'>
-      <Button variant='danger' onClick={() => closeConnection()}>Leave Room</Button>
+import './Chat.scss';
+
+const Chat = ({ messages, sendMessage, users, closeConnection }) => (
+  <div className="chat">
+    <div className="left">
+        <div className="message-cont">
+          <MessageContainer messages={messages} />
+          <SendMessageForm sendMessage={sendMessage} />
+       </div>
     </div>
-    <ConnectedUsers users={users} />
-    <div className='chat'>
-      <MessageContainer messages={messages} />
-      <SendMessageForm sendMessage={sendMessage} />
+    <div className="right">
+      <br></br>
+      <ConnectedUsers users={users} />
     </div>
+    
+    
   </div>
 );
 
