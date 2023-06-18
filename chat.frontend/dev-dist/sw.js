fed5e583466f301f6e074aad1a5e43aada037ab7
maroua <marouaamal2002@gmail.com>
2023-06-18 15:41:19 +0100

    c

--- chat.frontend/dev-dist/sw.js
+++ chat.frontend/dev-dist/sw.js
@@ -82,11 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-<<<<<<< HEAD
-    "revision": "0.qasndeamjig"
-=======
-    "revision": "0.79mo7bsmm4g"
->>>>>>> 13405deea859ce18825f1f355937f46a4d92b7d1
+    "revision": "0.afpfn6g6qr"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
