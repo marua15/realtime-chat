2764606780e495d6a761d9c27939369add35280b
HAITAME <haitame.laframe@etu.uae.ac.ma>
2023-06-18 13:38:25 +0100

    README

--- chat.frontend/dev-dist/sw.js
+++ chat.frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.733539nm4p"
+    "revision": "0.mft4ims4ndo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
